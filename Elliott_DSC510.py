df8c6eeffd981d72ee9bfde0f68476e758400f60
Belliott0526 <elliotfam94@gmail.com>
2025-06-07 16:11:04 -0500

    Add files via upload

---
+++ Elliott_DSC510.py
@@ -0,0 +1,6 @@
+#Program: DSC510-T301 Introduction to Programming (2257-1)
+#Week 1: Introduction to Programming, GIT, and PyCharm
+#Author: Barbara Elliott
+#Date: 06/05/2025
+
+print('hello world')
